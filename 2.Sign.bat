0cf6e4d50e034cc40d942e12d86e8d486eae5107
waywa <waywa@WAYNEWANGP70>
2017-01-29 23:20:47 +0800

    Sign script

---
+++ 2.Sign.bat
@@ -0,0 +1,4 @@
+
+
+rem  Sign
+"%ProgramFiles(x86)%\Windows Kits\10\bin\x64\signtool.exe" sign -f GreaterFileShare.UWP\GreaterFileShare.UWP_StoreKey.pfx -fd SHA256 -v UWPDesktopBridge\GreaterFileShare\GreaterFileShare.appx
