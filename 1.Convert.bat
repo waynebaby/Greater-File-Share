f8ead9fcd368f2b3c6f6ace92f745b9c83474fb0
waywa <waywa@WAYNEWANGP70>
2017-01-29 16:01:18 +0800

    Converter batch finished

---
+++ 1.Convert.bat
@@ -0,0 +1,4 @@
+
+
+rem Convert
+DesktopAppConverter.exe -Installer "GreaterFileShare.Installer\GreaterFileShare.Installer\Express\SingleImage\DiskImages\DISK1\setup.exe" -Destination UWPDesktopBridge  -Publisher "CN=4D2E4444-E4BB-4498-B89B-C3BC2D2D50F2" -Version 1.0.0.0 -InstallerArguments "/S /v/qn"  -AppExecutable ".\bin\GreaterFileShare.Hosts.WPF.exe" -PackageDisplayName "Greater File Share" -PackagePublisherDisplayName "Waynebaby Presents"  -PackageName "GreaterFileShare" -MakeAppx -Sign
