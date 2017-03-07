42c69e449e4e9862ce8afc8bd76b7db04512c5b1
waywa <waywa@WAYNEWANGP70>
2017-03-08 02:16:57 +0800

    1.0.3.0 submit second try

--- 1.Convert.bat
+++ 1.Convert.bat
@@ -1,4 +1,9 @@
 
 
-rem Convert
+@echo Converting...
+rem Following command is used for converter before 1.0.8.0
 DesktopAppConverter.exe -Installer "GreaterFileShare.Installer\GreaterFileShare.Installer\Express\SingleImage\DiskImages\DISK1\setup.exe" -Destination UWPDesktopBridge  -Publisher "CN=4D2E4444-E4BB-4498-B89B-C3BC2D2D50F2" -Version 1.0.3.0 -InstallerArguments "/S /v/qn"  -AppExecutable ".\bin\GreaterFileShare.Hosts.WPF.exe" -PackageDisplayName "Greater File Share" -PackagePublisherDisplayName "Waynebaby Presents"  -PackageName "57081WaynebabyPresents.GreaterFileShare" -AppId "App" -MakeAppx
+
+
+rem Following command is used for converter after 1.0.8.0 still not working.
+rem DesktopAppConverter.exe -Installer "D:\GitHub\GreaterFileShare\ReleaseFolder\bin" -Destination ".\UWPDesktopBridge"  -Publisher "CN=4D2E4444-E4BB-4498-B89B-C3BC2D2D50F2" -Version 1.0.3.0 -InstallerArguments "/S /v/qn"  -AppExecutable "GreaterFileShare.Hosts.WPF.exe" -PackageDisplayName "Greater File Share" -PackagePublisherDisplayName "Waynebaby Presents"  -PackageName "57081WaynebabyPresents.GreaterFileShare" -AppId "App" -MakeAppx
