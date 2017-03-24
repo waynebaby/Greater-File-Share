cbd45365c00e1ffe614fa1289218dcea7a51a6ba
waywa <waywa@WAYNEWANGP70>
2017-03-24 20:14:46 +0800

    Multilingual 1.0.5.0

--- 2.ExtractAndChange.bat
+++ 2.ExtractAndChange.bat
@@ -1,9 +1,39 @@
 
+
+set  configPath=UWPDesktopBridge\57081WaynebabyPresents.GreaterFileShare
+set  rp=UWPDesktopBridge\57081WaynebabyPresents.GreaterFileShare\ExtractedPackageFiles
+
+rem Incase There are gabage from last running,  delelte this dir.
 RD UWPDesktopBridge\57081WaynebabyPresents.GreaterFileShare\ExtractedPackageFiles /S /Q 
-powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('UWPDesktopBridge\57081WaynebabyPresents.GreaterFileShare\57081WaynebabyPresents.GreaterFileShare.appx', 'UWPDesktopBridge\57081WaynebabyPresents.GreaterFileShare\ExtractedPackageFiles'); }"
 
 
 
-BuildMe\bin\BuildMe.exe UWPDesktopBridge\57081WaynebabyPresents.GreaterFileShare\ExtractedPackageFiles\AppxManifest.xml
+rem Extract the converted packages
+powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%configPath%\57081WaynebabyPresents.GreaterFileShare.appx', '%rp%'); }"
 
-RD UWPDesktopBridge\57081WaynebabyPresents.GreaterFileShare\ExtractedPackageFiles\VFS\SystemX64\config\systemprofile\AppData\LocalLow\Microsoft\CryptnetUrlCache /S /Q 
+
+rem Copy Resource files here
+xcopy GreaterFileShare.UWP\Strings %configPath%\ExtractedPackageFiles\Strings /s   /y  /i
+
+
+
+
+rem dump all  file  into xml files
+rem for /f "delims=" %%a in ('dir/b /s %rp%\*.pri') do "%ProgramFiles(x86)%\Windows Kits\10\bin\x64\makepri" dump  /if "%%a"   /of "%%a.dump" 
+
+"%ProgramFiles(x86)%\Windows Kits\10\bin\x64\makepri" createconfig  /cf "%configPath%\resources.pri.xml"  /dq en-US_zh-Hans_zh-Hant /pv 10.0  /o
+
+"%ProgramFiles(x86)%\Windows Kits\10\bin\x64\makepri" new /pr "%rp%" /cf "%configPath%\resources.pri.xml" /of "%rp%\resources.pri" /mf AppX /o
+
+rem dump all  file  into xml files
+rem for /f "delims=" %%a in ('dir/b /s %rp%\*.pri') do "%ProgramFiles(x86)%\Windows Kits\10\bin\x64\makepri" dump  /if "%%a"    /of "%%a2.dump" 
+
+
+
+rem make more modifies. the logic is written in BuildMe\bin\BuildMe.exe. 
+BuildMe\bin\BuildMe.exe UWPDesktopBridge\57081WaynebabyPresents.GreaterFileShare\ExtractedPackageFiles\
+
+
+
+rem if there are gabage files blocked MakeAppx steps, we can deleted them here.
+rem RD UWPDesktopBridge\57081WaynebabyPresents.GreaterFileShare\ExtractedPackageFiles\VFS\SystemX64\config\systemprofile\AppData\LocalLow\Microsoft\CryptnetUrlCache /S /Q 
--- BuildMe/Program.cs
+++ BuildMe/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,23 +8,47 @@ using System.Xml.Linq;
 
 namespace BuildMe
 {
-   public  class Program
+    public class Program
     {
+        const string AppManifestFile = "AppxManifest.xml";
+
         static void Main(string[] args)
         {
-            ChangeName(args.FirstOrDefault());
+
+
+            Console.WriteLine("Changing AppManifest File Fields...");
+            ChangeAppManifestFileFields(args.FirstOrDefault());
+
+
         }
 
-        static void ChangeName(string filePath)
+        static void ChangeAppManifestFileFields(string folderPath)
         {
-            XDocument d =XDocument.Load(filePath);
-            var ve = d.Descendants().Where(x => x.Name.LocalName == "VisualElements").First();
-            ve.Attributes().Where(x => x.Name.LocalName == "DisplayName").First().Value = "Greater File Share";
-            ve.Attributes().Where(x => x.Name.LocalName == "Description").First().Value = "File Sharing Server With QR support";
+            void ChangeAttribute(XElement target, string field, string value)
+            {
+                target.Attributes().Where(x => x.Name.LocalName == field).First().Value = value;
+                Console.WriteLine($"{field} Changed to \"{ value}\"...");
+            }
+            var filepath = Path.Combine(folderPath, AppManifestFile);
 
-            
-            d.Save(filePath);
+            XDocument d = XDocument.Load(filepath);
+            var ve = d.Descendants().Where(x => x.Name.LocalName == "VisualElements").First();
+
+            ChangeAttribute(ve, "DisplayName", "ms-resource:Resources/AppName");
+            ChangeAttribute(ve, "Description", "ms-resource:Resources/AppDescription");
+
+
+            var rse = d.Descendants().FirstOrDefault(x => x.Name.LocalName == "Resources");
+            rse.Add(new XElement(
+                            XName.Get("Resource", rse.Name.NamespaceName),
+                            new XAttribute(XName.Get("Language"), "zh-Hans")));
+            rse.Add(new XElement(
+                   XName.Get("Resource", rse.Name.NamespaceName),
+                            new XAttribute(XName.Get("Language"), "zh-Hant")));
+
+            d.Save(filepath);
         }
-        
+
+
     }
 }
